e4fb0b20455f9910e1aa1bdbb7e7d3975ab30a6c
hschnoor <henning.schnoor@email.uni-kiel.de>
2016-11-25 11:26:12 +0100

    newer universe

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Universe.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Universe.java
@@ -52,7 +52,7 @@ public class Universe {
                         nextCell = DEAD_CELL;
                     }
                 } else {
-                    if (neighbourCount == 4) {
+                    if ( (neighbourCount == 3) || (neighbourCount == 4) ) {
                         nextCell = LIVE_CELL;
                     } else {
                         nextCell = DEAD_CELL;
